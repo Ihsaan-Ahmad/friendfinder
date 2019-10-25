71c5a01fc955120a8b3a767f8ba426a9faa6804d
Ihsaan Ahmad <ihsaan.edu4321@gmail.com>
2019-10-24 22:09:48 -0500

    updating files

---
+++ app/data/friends.js
@@ -0,0 +1,94 @@
+var friends = [
+    {
+        "name": "Sunkist",
+        "photo": "https://cdn.shopify.com/s/files/1/1511/0050/products/Sprite_1024x1024.png?v=1528134246",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+
+        ]
+    },
+
+    {
+        "name": "Sprite",
+        "photo": "https://cdn.shopify.com/s/files/1/1511/0050/products/Sprite_1024x1024.png?v=1528134246",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+
+        ]
+    },
+
+    {
+        "name": "Coke",
+        "photo": "https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/beveragedaily.com/article/2019/07/12/diet-coke-helps-boost-coca-cola-s-brand-value-brand-finance-rankings/9912727-1-eng-GB/Diet-Coke-helps-boost-Coca-Cola-s-brand-value-Brand-Finance-rankings_wrbm_large.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+
+        ]
+    },
+
+    {
+        "name": "Fanta",
+        "photo": "https://previews.123rf.com/images/darios/darios1405/darios140500233/28165384-fanta-drink-in-a-can-on-ice-isolated-on-white-background.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+
+        ]
+    },
+
+    {
+        "name": "Pepsi",
+        "photo": "https://www.pepsi.com/en-us/uploads/images/social-share.jpg?mtime=20180110134930",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+
+        ]
+    },
+
+];
+
+module.exports = friends;
